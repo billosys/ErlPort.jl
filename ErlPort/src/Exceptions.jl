d504e6fc953354665f8ee1a7facc59bbcfeffa78
Dreki Þórgísl <dreki@billo.systems>
2014-11-16 01:40:52 -0600

    Added custom exceptions module.

---
+++ ErlPort/src/Exceptions.jl
@@ -0,0 +1,15 @@
+type IncompleteData <: Exception
+    var
+end
+
+type UnknownProtocolVersion <: Exception
+    var
+end
+
+type InvalidCompressedTag <: Exception
+    var
+end
+
+type UnsupportedData <: Exception
+    var
+end
