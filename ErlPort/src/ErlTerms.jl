d5d4eb7e21f84eadf6cfc8267d7926483d072b7c
Dreki Þórgísl <dreki@billo.systems>
2014-11-16 01:41:07 -0600

    Added Atom type (might not use it).
    Also added some more implementation.

--- ErlPort/src/ErlTerms.jl
+++ ErlPort/src/ErlTerms.jl
@@ -26,16 +26,109 @@
 # POSSIBILITY OF SUCH DAMAGE.
 module ErlTerms
 
-export decode, IncompleteData
+export IncompleteData, UnknownProtocolVersion, InvalidCompressedTag,
+UnsupportedData,
+decode, getindex, length
 
-type IncompleteData <: Exception end
+import Base.length
 
-function decode(string::ASCIIString)
-    string
+include("Exceptions.jl")
+
+type Atom
+  bytes::Array{Uint8,1}
+end
+
+Atom() = Atom(b"")
+
+getindex(atom::Atom, index::Int64) = getindex(atom.bytes, index)
+getindex(atom::Atom, range::UnitRange{Int64}) = getindex(atom.bytes, range)
+length(atom::Atom) = Base.length(atom.bytes)
+
+function decode(bytes::Array{Uint8,1})
+    if length(bytes) == 0
+        throw(IncompleteData(bytes))
+    end
+    if bytes[1] != 131
+        throw(UnknownProtocolVersion(bytes[1]))
+    end
+    if length(bytes) < 4
+        throw(IncompleteData(bytes))
+    end
+    if bytes[2] == b"P"
+        # compressed term
+        if length(bytes) < 16
+            throw(IncompleteData(bytes))
+        end
+        # XXX add support for decompressing
+    end
+    decodeterm(bytes[2:end])
 end
 
 function decode(unsupported)
-    throw(IncompleteData())
+    throw(IncompleteData(unsupported))
 end
+
+
+function decodeterm(bytes::Array{Uint8,1})
+    if length(bytes) == 0
+        throw(IncompleteData(bytes))
+    end
+    tag = bytes[1]
+    if tag == 100
+        # ATOM_EXT
+        return decodeatom(bytes)
+    elseif tag == 106
+        # NIL_EXT
+        return bytes
+    elseif tag == 107
+        # STRING_EXT
+        return bytes
+    elseif tag in b"lhi"
+        # LIST_EXT, SMALL_TUPLE_EXT, LARGE_TUPLE_EXT
+        return bytes
+    elseif tag == 97
+        # SMALL_INTEGER_EXT
+        return bytes
+    elseif tag == 98
+        # INTEGER_EXT
+        return bytes
+    elseif tag == 109
+        # BINARY_EXT
+        return bytes
+    elseif tag == 70
+        # NEW_FLOAT_EXT
+        return bytes
+    elseif tag in b"no"
+        # SMALL_BIG_EXT, LARGE_BIG_EXT
+        if tag == 110
+            return bytes
+        end
+        return bytes
+    else
+        throw(UnsupportedData(bytes))
+    end
+end
+
+function decodeatom(bytes::Array{Uint8,1})
+    ln = length(bytes)
+    if ln < 3
+        throw(IncompleteData(bytes))
+    end
+    unpackedln = 0
+    if ln < unpackedln
+        throw(IncompleteData(bytes))
+    end
+    name = bytes[3:unpackedln]
+    if name == b"true"
+        return true, bytes[unpackedln:end]
+    elseif name == b"false"
+        return false, bytes[unpackedln:end]
+    elseif name == b"undefined"
+        return None, bytes[unpackedln:end]
+    else
+        return symbol(name), bytes[unpackedln:end]
+    end
+end
+
 
 end
