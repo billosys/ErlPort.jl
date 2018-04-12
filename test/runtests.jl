8e879e874f0514e98316061b111a603c30579f52
Csaba Hoch <csaba@cursorinsight.com>
2018-04-12 15:28:12 +0200

    Update to Julia 0.6
    Julia 0.6 introduced a stricter check for dynamically included code,
    which made the previous runtests.jl throw the following error:
    
         run() at /home/hcs/w/julia6/v0.6/ErlPort/test/CLITest.jl:6
         (method too new to be called from this world context.)
    
    So runtests.jl has been modified to eliminate this error.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,20 @@
-module ErlPortTests
-
 const FILES = String["CLITest.jl",
                      "DecodeTest.jl",
                      "EncodeTest.jl"
                     ]
 
-function run()::Void
-    run(FILES)
+function run_test(test_module::Module)::Void
+    Base.invokelatest(test_module.run)
+
+    return nothing
 end
 
-function run(files::Vector{String})::Void
-    for file in files
-        let test_module::Module = include(file)
-            test_module.run()
-        end
+function run_tests(test_modules::Vector{Module})::Void
+    for test_module in test_modules
+        run_test(test_module)
     end
+
+    return nothing
 end
 
-end # module
-
-ErlPortTests.run()
+run_tests([include(test_module_path) for test_module_path in FILES])
